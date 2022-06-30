f1e3006722e42b1c85e58df50b733c30bf73a312
kewinzaq1 <kewinszlezingier@gmail.com>
2022-06-30 16:08:22 +0200

    add jokemodal

---
+++ src/components/JokeModal.tsx
@@ -0,0 +1,49 @@
+import {useState} from 'react'
+import {Button, Modal, Paper, Typography} from '@mui/material'
+import {useJokes} from '../context'
+
+export const JokeModel = () => {
+  const {joke} = useJokes()
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+  return (
+    <div>
+      {joke && (
+        <Button
+          onClick={handleOpen}
+          variant={'contained'}
+          style={{marginTop: 10}}
+        >
+          Show Joke
+        </Button>
+      )}
+      <Modal
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="joke-modal-title"
+        aria-describedby="joke-modal-description"
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Paper
+          sx={{
+            background: '#fff',
+            padding: '1rem',
+            maxWidth: '400px',
+            borderRadius: '.25rem'
+          }}
+        >
+          <Typography id="joke-modal-title" variant="h6" component="h2">
+            {joke?.value}
+          </Typography>
+        </Paper>
+      </Modal>
+    </div>
+  )
+}
